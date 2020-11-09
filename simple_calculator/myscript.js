f3e3b13c814215d463c23e746f6aa5ef62eed51f
JustaKim <rlaruddlf711@naver.com>
2020-11-09 23:30:27 +0900

    Add restart function

--- simple_calculator/myscript.js
+++ simple_calculator/myscript.js
@@ -2,10 +2,16 @@ const showNumber = document.getElementById("result")
 let resultNumber = Number(showNumber.innerText)
 
 document.addEventListener("click", function(e) {
-    console.log("clicked")
+    
     if (e.target.className.includes("cal-button")) {
+        console.log("cal-button clicked")
         resultNumber = resultNumber + Number(e.target.innerText)
         showNumber.innerText = resultNumber
         console.log(showNumber.innerText)
     }
+
+    if (e.target.className.includes("reset")) {
+        console.log("reset clicked")
+        resultNumber = Number(0);
+        }
 })
