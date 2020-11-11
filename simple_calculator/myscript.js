8f93505fbff3cdf4f8a7cfb162084e688ca71fed
JustaKim <rlaruddlf711@naver.com>
2020-11-11 09:20:22 +0900

    Seperate reset function

--- simple_calculator/myscript.js
+++ simple_calculator/myscript.js
@@ -6,14 +6,27 @@ document.addEventListener("click", function(e) {
     
     if (e.target.className.includes("cal-button")) {
         console.log("cal-button clicked")
-        resultNumber = resultNumber + Number(e.target.innerText)
+        resultNumber = e.target.innerText
         showNumber.innerText = resultNumber
         console.log(showNumber.innerText)
     }
 
     if (e.target.id.includes("reset")) {
         console.log("reset clicked")
-        resultNumber = 0;
-        showNumber.innerText = resultNumber;
+        reset();
+    }
+
+    if (e.target.id.includes("plus")) {
+        
     }
 })
+
+function reset() {
+    resultNumber = 0
+    showNumber.innerText = resultNumber
+}
+
+function sum(a, b) {
+    resultNumber = a+b
+    showNumber.innerText = resultNumber
+}
